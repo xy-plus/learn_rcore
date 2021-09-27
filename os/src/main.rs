68b85f238313a964346032bf55143d2807e1480a
小源 <511568816@qq.com>
2021-09-27 18:35:50 +0800

    ch1

---
+++ os/src/main.rs
@@ -0,0 +1,27 @@
+#![no_main]
+#![no_std]
+#![feature(global_asm)]
+#![feature(llvm_asm)]
+#![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+
+global_asm!(include_str!("entry.asm"));
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("hello world");
+    panic!("Shutdown machine!");
+}
